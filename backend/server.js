2427c0a775aeb5591e6c9502e2e6fb9b8106a05a
Dawit575 <dawittadesse56@gmail.com>
2026-05-16 01:27:52 +0300

    I add router

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
-import express from "express";
-import dotenv from "dotenv";
-import './config/db.js';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
+import './config/db.js';
+import router  from './routes/authRoutes.js';
+
 const app = express();
 
-const port = process.PORT || 3000;
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/auth-routes', router);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`The server is running on http://localhost:${port}`);
