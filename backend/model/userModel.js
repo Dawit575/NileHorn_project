b06e5e0b4f85bddd788a95ce257d694cc5cf2116
Dawit575 <dawittadesse56@gmail.com>
2026-05-16 00:34:52 +0300

    I add createUser Function

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,6 +1,13 @@
 import db from "../config/db";
 
-export const findUserByEmail = (email,callback) => {
+
+export const createUser = (user_name,user_email,hashedPassword,callback) => {
+  const sql  = `INSERT INTO users (user_name,user_email,user_password)
+  VALUES(?, ?, ?);`;
+  db.query(sql,[user_name,user_email,hashedPassword],callback)
+}
+
+export const findUserByEmail = (user_email,callback) => {
   const sql = `SELECT * FROM users WHERE user_email=?;`;
-  db.query(sql,[email],callback);
+  db.query(sql,[user_email],callback);
 }
