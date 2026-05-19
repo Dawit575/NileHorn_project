7e84ae1d3367645368f9eb465de6b2bb11edab68
Dawit575 <dawittadesse56@gmail.com>
2026-05-19 21:47:11 +0300

    I removed comment line

--- backend/model/portfolioModel.js
+++ backend/model/portfolioModel.js
@@ -1,18 +1,16 @@
 import db from "../config/db.js";
 
-// get All portfolio
 export const getAllPortfolio = (callback) => {
   const sql = `SELECT * FROM portfolio;`;
   db.query(sql,callback);
 }
 
-//get portfolio by ID
 export const getPortfolioById = (id,callback) => {
   const sql = `SELECT * FROM portfolio WHERE id=?;`;
   db.query(sql,[id],callback);
 }
 
-//create Portfolio
+
 export const createPortfolio = (data,callback) => {
   const sql = `INSERT INTO portfolio (title,description,image,github_link,live_link)
                 VALUES(?, ?, ?, ?, ?);`;
@@ -25,7 +23,7 @@ export const createPortfolio = (data,callback) => {
                 ],callback);
 }
 
-//update portfolio
+
 export const updatePortfolio = (id,data,callback) => {
   const sql = `UPDATE portfolio SET title=?, description=?, image=?, github_link=?, live_link=?
               WHERE id=?;`;
@@ -39,7 +37,6 @@ export const updatePortfolio = (id,data,callback) => {
               ],callback);
 }
 
-// delete portfolio
 export const deletePortfolio  = (id,callback) => {
   const sql = `DELETE FROM portfolio WHERE id=?;`;
             db.query(sql,[id],callback);
