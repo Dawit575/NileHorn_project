eb38707edfc64eb261c2457e59ee2091d8bf132f
Dawit575 <dawittadesse56@gmail.com>
2026-05-19 19:06:28 +0300

    I protected my backend routes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -8,14 +8,14 @@ export const verifyAdmmin = (req,res,next) => {
   const token = authHeader.split(" ")[1];
 
   if(!token){
-    return req.status(401).json({
+    return res.status(401).json({
       message:"Token is not provided."
     })
   }
 
   try {
     const verified = jwt.verify(token,process.env.DB_SECRET);
-    
+
     req.user = verified;
     next();
   } catch (error) {
