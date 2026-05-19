eb75d906a025fcf3d0e133842bef41442f566540
Dawit575 <dawittadesse56@gmail.com>
2026-05-19 14:17:12 +0300

    I add portfolio controller

---
+++ backend/controller/portfolioController.js
@@ -0,0 +1,63 @@
+import { getAllPortfolio,getPortfolioById,createPortfolio,updatePortfolio,deletePortfolio } from "../model/portfolioModel.js";
+
+export const getAll = (req,res) => {
+  getAllPortfolio((error, result) => {
+    if(error) {
+      return res.status(500).json({ message: 'Database error', error });
+    }
+    return res.status(200).json(result);
+  })
+}
+
+export const getById = (req,res) => {
+  getPortfolioById(req.params.id,(error,result) => {
+    if (error) {
+      return res.status(500).json({ message: 'Database error', error });
+    }
+
+    if(result.length === 0){
+      return res.status(404).json({ message: 'Portfolio is not found.' });
+    }
+
+    return res.status(200).json(result[0]);
+  })
+}
+
+export const addPortfolio = (req,res) => {
+  createPortfolio(req.body,(error,result) =>{
+    if (error) {
+      return res.status(500).json({ message: 'Database error', error });
+    }
+
+    return res.status(201).json({
+      message:"Your portfolio is created successfully.",
+      id:result.insertId
+    })
+  })
+}
+
+export const editPortfolio = (req,res) => {
+  updatePortfolio(req.params.id,req.body,(error,result) => {
+    if (error) {
+      return res.status(500).json({ message: 'Database error', error });
+    }
+
+    if(result.affectedRows === 0){
+      return res.status(404).json({ message: 'Portfolio is not found.' });
+    }
+
+    return res.status(201).json({message:"Your portfolio is updated successfully."})
+  })
+}
+
+export const delete_portfolio = (req,res) => {
+    if (error) {
+      return res.status(500).json({ message: 'Database error', error });
+    }
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: 'Portfolio is not found.' });
+    }
+
+    return res.status(201).json({message:"Portfolio deleted successfully."})
+}
