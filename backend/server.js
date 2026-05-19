27b36605f8680ba7a8d6df227a171c059e3f04ac
Dawit575 <dawittadesse56@gmail.com>
2026-05-19 14:20:03 +0300

    I add portfolio on server

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 import './config/db.js';
 import router  from './routes/authRoutes.js';
+import portfolioRoutes from './routes/portfolioRoutes.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/auth-routes', router);
+app.use('/api/portfolio-routes',portfolioRoutes);
 
 const port = process.env.PORT || 3000;
 
