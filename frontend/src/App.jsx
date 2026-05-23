4f59652f80f8adbdf54676b6bfd7eff7f17f5c73
Dawit575 <dawittadesse56@gmail.com>
2026-05-23 14:57:25 +0300

    I commit some routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,10 @@ import About from "./pages/public_pages/About/About.jsx";
 import Services from "./pages/public_pages/Services/Service.jsx";
 import Portfolio from './pages/public_pages/Portfolio/Portfolio.jsx';
 import contact from "./pages/public_pages/Contact/Contact.jsx";
-import login from "./pages/public_pages/Login/Login.jsx"
+import login from "./pages/public_pages/Login/Login.jsx";
+import Dashboard_layout from './components/Layout/Dashoard_Layout/Dashboard_layout.jsx';
+import Dashboard from "./pages/admin_page/Dasboard/Dashboard.jsx"
+
 function App() {
   return (
     <Routes>
@@ -17,6 +20,10 @@ function App() {
           <Route path='/portfolio' element = {<Portfolio/>}/>
           <Route path='/contact' element = {<contact/>}/>
           <Route path='/login' element = {<login/>}/>
+      </Route>
+
+      <Route path='/admin' element = {<Dashboard_layout/>}>
+          <Route index element = {<Dashboard/>}></Route>
       </Route>
     </Routes>
   )
