c60d52c7a5f14435a2a14caacd4f84cd04a92936
Dawit575 <dawittadesse56@gmail.com>
2026-05-24 01:53:25 +0300

    I commit admin components

--- frontend/src/components/Admin/Dashboard/Sidebar/Sidebar.jsx
+++ frontend/src/components/Admin/Dashboard/Sidebar/Sidebar.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./Sidebar.module.css";
 import { Link } from 'react-router-dom';
 
 function Sidebar() {
+  const [menuOpen,setMenuOpen] = useState(false)
   return (
-    <div className={styles.sidebar}>
+    <div className={`${styles.sidebar} ${menuOpen ? styles.activeSidebar : ''}`}>
+
+      <div className={styles.hamburger} onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? '✕' : '☰'}
+      </div>
+
       <h1 className={styles.title}>Admin</h1>
-      <div className= {styles.admin_sidebar}>
-          <button className= {styles.admin_dashboard_link} to="/admin">Dashboard</button>
-          <Link to= '/' className={styles.logout_btn}>Logout</Link>
+      
+      <div className={styles.admin_sidebar}>
+        <button className={styles.admin_dashboard_link} to="/admin">
+          Dashboard
+        </button>
+        <Link to="/" className={styles.logout_btn}>
+          Logout
+        </Link>
       </div>
     </div>
   );
