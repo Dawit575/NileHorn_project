ca97a01ef1f19b0522d954e9490f40e19fcedb9d
Dawit575 <dawittadesse56@gmail.com>
2026-05-24 16:21:12 +0300

    I commit the hero image with css

--- frontend/src/components/Commen/Hero/Hero.jsx
+++ frontend/src/components/Commen/Hero/Hero.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import style from './Hero.module.css';
 import { Link } from 'react-router-dom';
+import hero_image from "../../../assets/images/edited.png";
 
 function Hero() {
   return (
     <section className={style.hero}>
       <div className={style.overlay}></div>
 
-      <div className={style.content}>
-        <h1>
-          Hi, I'm <span>Dawit</span>
-        </h1>
+      <div className={style.hero_content}>
+        <div className={style.left_content}>
 
-        <h2>I build fast, modern web experiences people actually enjoy using</h2>
+          <div className={style.left_text}>
+            <h1>
+              Hi, I'm <span>Dawit</span>
+            </h1>
+            <h2>I build fast, modern web experiences people actually enjoy using</h2>
+          </div>
 
-        <div className={style.buttons}>
-          <Link to="/portfolio" className={style.primaryBtn}>
-            See Portfolio
-          </Link>
-          <Link to="/service" className={style.secondaryBtn}>
-            Service
-          </Link>
+          <div className={style.left_buttons}>
+            <Link to="/portfolio" className={style.primaryBtn}>
+              See Portfolio
+            </Link>
+            <Link to="/service" className={style.secondaryBtn}>
+              Service
+            </Link>
+          </div>
+        </div>
+
+        <div className={style.right_content}>
+          <img src={hero_image} alt="Hero Banner" />
         </div>
       </div>
     </section>
