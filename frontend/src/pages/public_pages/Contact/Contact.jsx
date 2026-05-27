d9b2a47d98a65ef78282159643d1888d2534e43a
Dawit575 <dawittadesse56@gmail.com>
2026-05-28 01:47:29 +0300

    commited

--- frontend/src/pages/public_pages/Contact/Contact.jsx
+++ frontend/src/pages/public_pages/Contact/Contact.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Contact_Form from '../../../components/Contact/Contact_Form/Contact_Form';
+import Contact_info from '../../../components/Contact/Contact_Information/Contact_Info';
+import Social_Links from '../../../components/Contact/Social_Links/Social_Links';
+import style from './Contact.module.css';
 
 function Contact() {
   return (
-    <div>
-      
+    <div className={style.container}>
+      <h1 className={style.title}>Contact Me</h1>
+
+      <div className={style.content}>
+
+        <div className={style.formSection}>
+          <Contact_Form />
+        </div>
+
+        <div className={style.infoSection}>
+          <Contact_info />
+        </div>
+      </div>
+
+      <div className={style.socialSection}>
+        <Social_Links />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
