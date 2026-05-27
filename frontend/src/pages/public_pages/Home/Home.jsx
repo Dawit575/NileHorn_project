cfecb997bf086b99925e76627ef617694ab218d0
Dawit575 <dawittadesse56@gmail.com>
2026-05-27 11:22:38 +0300

    Home page commited

--- frontend/src/pages/public_pages/Home/Home.jsx
+++ frontend/src/pages/public_pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from '../../../components/Commen/Hero/Hero'
+import Hero from '../../../components/Commen/Hero/Hero';
 function Home() {
   return (
     <div>
