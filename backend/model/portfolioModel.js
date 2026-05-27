7ac74489f0c5de1ce6364fc630c75c61a06c34b1
Dawit575 <dawittadesse56@gmail.com>
2026-05-27 19:15:24 +0300

    I commit portfolio model

--- backend/model/portfolioModel.js
+++ backend/model/portfolioModel.js
@@ -11,33 +11,39 @@ export const getPortfolioById = (id,callback) => {
 }
 
 
-export const createPortfolio = (data,callback) => {
-  const sql = `INSERT INTO portfolio (title,description,image,github_link,live_link)
-                VALUES(?, ?, ?, ?, ?);`;
-                db.query(sql, [
-                  data.title,
-                  data.description,
-                  data.image,
-                  data.github_link,
-                  data.live_link,
-                ],callback);
-}
+export const createPortfolio = (data, callback) => {
+
+  const sql = `
+    INSERT INTO portfolio (title, description, image, github_link, live_link)
+    VALUES (?, ?, ?, ?, ?)
+  `;
+
+  db.query(
+    sql,
+    [data.title, data.description, data.image, data.github_link, data.live_link],
+    callback
+  );
+};
 
 
-export const updatePortfolio = (id,data,callback) => {
-  const sql = `UPDATE portfolio SET title=?, description=?, image=?, github_link=?, live_link=?
-              WHERE id=?;`;
-              db.query(sql,[
-              data.title,
-              data.description,
-              data.image,
-              data.github_link,
-              data.live_link,
-              id
-              ],callback);
-}
+export const updatePortfolio = (id, data, callback) => {
 
-export const deletePortfolio  = (id,callback) => {
-  const sql = `DELETE FROM portfolio WHERE id=?;`;
-            db.query(sql,[id],callback);
-}
+  const sql = `
+    UPDATE portfolio
+    SET title = ?, description = ?, image = ?, github_link = ?, live_link = ?
+    WHERE id = ?
+  `;
+
+  db.query(
+    sql,
+    [data.title, data.description, data.image, data.github_link, data.live_link, id],
+    callback
+  );
+};
+
+export const deletePortfolio = (id, callback) => {
+
+  const sql = `DELETE FROM portfolio WHERE id = ?`;
+
+  db.query(sql, [id], callback);
+};
