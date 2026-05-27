b1ef0633300d62fba1bbdd63f45932609e3e22b0
Dawit575 <dawittadesse56@gmail.com>
2026-05-27 11:17:47 +0300

    app.jsx commited

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,28 +5,35 @@ import Home from "./pages/public_pages/Home/Home.jsx";
 import About from "./pages/public_pages/About/About.jsx";
 import Services from "./pages/public_pages/Services/Service.jsx";
 import Portfolio from './pages/public_pages/Portfolio/Portfolio.jsx';
-import contact from "./pages/public_pages/Contact/Contact.jsx";
-import login from "./pages/public_pages/Login/Login.jsx";
+import Contact from "./pages/public_pages/Contact/Contact.jsx";
+import Login from "./pages/public_pages/Login/Login.jsx";
 import Dashboard_layout from './components/Layout/Dashoard_Layout/Dashboard_layout.jsx';
 import Dashboard from "./pages/admin_page/Dasboard/Dashboard.jsx"
-
+import ProtectedRoute from './routes/ProtectedRoutes.jsx';
 function App() {
   return (
     <Routes>
-      <Route element = {<Public_Layout/>}>
-          <Route path='/' element = {<Home/>}/>
-          <Route path='/about' element = {<About/>}/>
-          <Route path='/service' element = {<Services/>}/>
-          <Route path='/portfolio' element = {<Portfolio/>}/>
-          <Route path='/contact' element = {<contact/>}/>
-          <Route path='/login' element = {<login/>}/>
+      <Route element={<Public_Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Services />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
       </Route>
 
-      <Route path='/admin' element = {<Dashboard_layout/>}>
-          <Route index element = {<Dashboard/>}></Route>
+      <Route path="/admin" element={<Dashboard_layout />}>
+        <Route
+          index
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        ></Route>
       </Route>
     </Routes>
-  )
+  );
 }
 
 export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AuthProvider } from './contexts/Auth_context.jsx';
+import { PortfolioProvider } from './contexts/Portfolio_context.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <PortfolioProvider>
+          <App />
+        </PortfolioProvider>
+        <ToastContainer />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
