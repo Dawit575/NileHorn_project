419104d1e821e46d02445c12b490684b07aa63a9
Dawit575 <dawittadesse56@gmail.com>
2026-05-27 21:46:29 +0300

    I commited About.jsx

--- frontend/src/pages/public_pages/About/About.jsx
+++ frontend/src/pages/public_pages/About/About.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-import Hero from '../../../components/Commen/Hero/Hero'
+import Hero from '../../../components/Commen/Hero/Hero';
+import About_info from '../../../components/About/About_Informations/About_Info';
+import Education from '../../../components/About/Educations/Education';
+import Experiance from '../../../components/About/Experiance/Experiance';
+import Mission_vision from '../../../components/About/Mission_Vision/Mission';
+import Skills from '../../../components/About/Skills/Skills.jsx';
 function About() {
   return (
     <div>
       <Hero/>
+      <About_info/>
+      <Education/>
+      <Experiance/>
+      <Mission_vision/>
+      <Skills/>
     </div>
   )
 }
